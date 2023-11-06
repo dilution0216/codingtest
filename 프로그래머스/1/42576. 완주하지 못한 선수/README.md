bc4cba66e6e8e753dc6605e36353c05777c567cf
Huiseok Kim(김희석) <57430885+dilution0216@users.noreply.github.com>
2023-11-07 04:57:28 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 63.90 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
 
 ### 성능 요약
 
-메모리: 37.6 MB, 시간: 45.90 ms
+메모리: 38.9 MB, 시간: 63.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 2일 4:53:51
+2023년 11월 2일 4:57:25
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,13 +1,6 @@
+import collections
+
+
 def solution(participant, completion):
-    answer = ''
-    hash_dict={} # hash dictionary
-    sumHash=0
-    for i in participant:
-        hash_dict[hash(i)]=i # participant hash인덱스 넣기
-        sumHash+=hash(i) # hash 총합
-        
-        
-    for j in completion:
-        sumHash-=hash(j) # completion의 hash값들 빼기
-        
-    return hash_dict[sumHash] # 남은 hash값에 해당하는 선수가 완주 못한 선수
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
