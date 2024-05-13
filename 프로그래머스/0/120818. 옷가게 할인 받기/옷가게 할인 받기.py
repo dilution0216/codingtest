757c5c9272e44df2764f6097ad31752eb4822aa5
Huiseok Kim(김희석) <57430885+dilution0216@users.noreply.github.com>
2024-05-13 19:12:19 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,11 @@
+def solution(price):
+    answer=0
+    if price>=500000:
+        answer=price*0.8
+    elif 500000>price>=300000:
+        answer=price*0.9
+    elif 300000>price>=100000:
+        answer=price*0.95
+    elif price<100000:
+        answer=price
+    return int(answer)
