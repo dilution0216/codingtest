cf48607af3f7e8cfdfe018611103142896627512
Huiseok Kim(김희석) <57430885+dilution0216@users.noreply.github.com>
2024-06-12 15:07:44 +0900

    Time: 108 ms (21.97%), Space: 17.8 MB (7.43%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
@@ -3,10 +3,7 @@ class Solution:
         # 1. 2D 행렬을 1D 리스트로 변환
         flat_list = [num for row in grid for num in row]
         
-        # 2. 리스트를 정렬
-        flat_list.sort()
-        
-        # 3. 음수의 개수 세기
+        # 2. 음수의 개수 세기
         negative_count = sum(1 for num in flat_list if num < 0)
         
         return negative_count
