a81bd9f8d978f927240344d58f38baea82d9ae15
Huiseok Kim(김희석) <hglad0216@naver.com>
2024-08-01 13:43:18 +0900

    [Silver V] Title: 두라무리 휴지, Time: 96 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Silver/25178. 두라무리 휴지/두라무리 휴지.py
+++ 백준/Silver/25178. 두라무리 휴지/두라무리 휴지.py
@@ -4,31 +4,32 @@ def count_words(word):
     return Counter(word)
 
 def remove_nouns_from_word(word):
-    nouns = ['a', 'e', 'i', 'o', 'u']
-    return "".join([w for w in list(word) if w not in nouns])
-
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    return "".join([w for w in list(word) if w not in vowels])
 
 def duramuri_huji(word1, word2):
     answer = "NO"
-    cnt1, cnt2 = count_words(word=word1), count_words(word=word2)
+    cnt1, cnt2 = count_words(word1), count_words(word2)
     
-    word1_non_noun = remove_nouns_from_word(word=word1)
-    word2_non_noun = remove_nouns_from_word(word=word2)
+    word1_non_vowel = remove_nouns_from_word(word1)
+    word2_non_vowel = remove_nouns_from_word(word2)
     
     condition1 = cnt1 == cnt2
     condition2 = (word1[0] == word2[0]) and (word1[-1] == word2[-1])
-    condition3 = word1_non_noun == word2_non_noun
-    
+    condition3 = word1_non_vowel == word2_non_vowel
     
     if condition1 and condition2 and condition3:
         answer = "YES"
         
     return answer
 
-
 if __name__ == "__main__":
-    N = int(input())
-    word1 = input()
-    word2 = input()
+    import sys
+    input = sys.stdin.read
+    data = input().split()
     
-    print(duramuri_huji(word1=word1, word2=word2))
+    N = int(data[0])
+    word1 = data[1]
+    word2 = data[2]
+    
+    print(duramuri_huji(word1, word2))
