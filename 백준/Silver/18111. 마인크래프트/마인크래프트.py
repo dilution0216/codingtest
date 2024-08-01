c6aeb7a248296a751f3f2dc9ff7cfb6e70c24cab
Huiseok Kim(김희석) <hglad0216@naver.com>
2024-08-01 14:02:45 +0900

    [Silver II] Title: 마인크래프트, Time: 136 ms, Memory: 50284 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,37 +1,55 @@
-#입력받기
-MAX_HEIGHT = 256
-MIN_HEIGHT = 0
-N, M, B = map(int, input().split())
-board = [ list(map(int, input().split())) for _ in range(N)]
-height_cnt = [0 for _ in range(MAX_HEIGHT + 1)]
-answer_cost = 100000001
-answer_height = 0
-sum_height = B 
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    
+    MAX_HEIGHT = 256
+    MIN_HEIGHT = 0
+    N = int(data[0])  # 땅의 세로 크기
+    M = int(data[1])  # 땅의 가로 크기
+    B = int(data[2])  # 인벤토리 블록의 수
+    
+    ground = []
+    index = 3
+    for i in range(N):
+        row = []
+        for j in range(M):
+            row.append(int(data[index + j]))
+        ground.append(row)
+        index += M
+    
+    height_cnt = [0 for _ in range(MAX_HEIGHT + 1)]
+    answer_cost = float('inf')
+    answer_height = 0
+    sum_height = B
 
-#각 높이의 개수를 저장하는 리스트 입력받기 
-for row in board:
-    for val in row:
-        height_cnt[val] += 1
-        sum_height += val
-     
-#가능한 높이값들을 저장
-min_h = min([ min(row) for row in board ])
-max_h = sum_height // (M * N)
+    # 각 높이의 개수를 저장하는 리스트 입력받기
+    for row in ground:
+        for val in row:
+            height_cnt[val] += 1
+            sum_height += val
+    
+    # 가능한 높이값들을 저장
+    min_h = min(map(min, ground))
+    max_h = sum_height // (N * M)
 
-#각 높이를 평탄화 하는데 걸리는 시간 탐색미
-for h in range(min_h, max_h+1):
-    cost = 0
-    usable_blocks = B
-    for i in range(MIN_HEIGHT, MAX_HEIGHT+1 ):
-        if h < i:
-            cost += height_cnt[i] * abs(h - i) * 2
-            usable_blocks += height_cnt[i] * abs(h - i)
-        else:
-            cost += height_cnt[i] * abs(h - i)
-            usable_blocks -= height_cnt[i] * abs(h - i)
-            
-    if cost <= answer_cost and usable_blocks >= 0:
-        answer_cost = cost
-        answer_height = h
+    # 각 높이를 평탄화 하는데 걸리는 시간 탐색
+    for h in range(min_h, max_h + 1):
+        cost = 0
+        usable_blocks = B
+        for i in range(MIN_HEIGHT, MAX_HEIGHT + 1):
+            if h < i:
+                cost += height_cnt[i] * (i - h) * 2
+                usable_blocks += height_cnt[i] * (i - h)
+            else:
+                cost += height_cnt[i] * (h - i)
+                usable_blocks -= height_cnt[i] * (h - i)
 
-print(answer_cost, answer_height)
+        if usable_blocks >= 0 and cost <= answer_cost:
+            answer_cost = cost
+            answer_height = h
+
+    print(answer_cost, answer_height)
+
+if __name__ == "__main__":
+    main()
